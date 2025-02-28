6cb911823c19cf98e849650f752953b782a86bc9
Ryan DJ <ryan.djoenaidi@akqa.com>
2025-02-28 12:51:13 +0700

    Update meter clock styling

--- scripts/clocks/meter.js
+++ scripts/clocks/meter.js
@@ -3,6 +3,8 @@ import {
   getMinuteDialDegree,
   getHourDialDegree,
 } from "../utilities/dials.js";
+import { padZeros } from "../utilities/numbers.js";
+import { getDayShort, getMonthShort } from "../utilities/strings.js";
 
 const render = () => {
   // Get main clock component
@@ -22,6 +24,43 @@ const render = () => {
   const digitalFrame = document.createElement("div");
   digitalFrame.className = "meter__frame--digital";
 
+  // Create clock grids
+  for (let i = 0; i < 120; i++) {
+    const second = i + 1;
+    const grid = document.createElement("div");
+    grid.classList.add("meter__grid");
+    grid.classList.add("meter__grid--second");
+    if (second % 10 == 0) {
+      grid.classList.add("meter__grid--light");
+    }
+    grid.style.transform = `rotate(${second * 3}deg)`;
+    secondFrame.append(grid);
+  }
+
+  for (let i = 0; i < 120; i++) {
+    const second = i + 1;
+    const grid = document.createElement("div");
+    grid.classList.add("meter__grid");
+    grid.classList.add("meter__grid--minute");
+    if (second % 10 == 0) {
+      grid.classList.add("meter__grid--light");
+    }
+    grid.style.transform = `rotate(${second * 3}deg)`;
+    minuteFrame.append(grid);
+  }
+
+  for (let i = 0; i < 120; i++) {
+    const second = i + 1;
+    const grid = document.createElement("div");
+    grid.classList.add("meter__grid");
+    grid.classList.add("meter__grid--hour");
+    if (second % 10 == 0) {
+      grid.classList.add("meter__grid--light");
+    }
+    grid.style.transform = `rotate(${second * 3}deg)`;
+    hourFrame.append(grid);
+  }
+
   // Create clock dials
   const hourDial = document.createElement("div");
   hourDial.classList.add("meter__dial--hour");
@@ -39,6 +78,32 @@ const render = () => {
   minuteFrame.append(hourFrame);
   hourFrame.append(hourDial);
   hourFrame.append(digitalFrame);
+
+  // Create digital clock
+  const digital = document.createElement("div");
+  digital.classList.add("meter__digital");
+  digitalFrame.append(digital);
+
+  const digitalHour = document.createElement("div");
+  digitalHour.classList.add("meter__digital__hour");
+  digital.append(digitalHour);
+
+  const digitalSecond = document.createElement("div");
+  digitalSecond.classList.add("meter__digital__second");
+  digital.append(digitalSecond);
+
+  const digitalMinute = document.createElement("div");
+  digitalMinute.classList.add("meter__digital__minute");
+  digital.append(digitalMinute);
+
+  const digitalDate = document.createElement("div");
+  digitalDate.classList.add("meter__digital__date");
+  digital.append(digitalDate);
+
+  // Create glass
+  const glass = document.createElement("div");
+  glass.classList.add("meter__glass");
+  clock.append(glass);
 };
 
 const start = (time) => {
@@ -46,6 +111,14 @@ const start = (time) => {
   const smoothSecondDialDegree = getSmoothSecondDialDegree(time);
   const minuteDialDegree = getMinuteDialDegree(time);
   const hourDialDegree = getHourDialDegree(time);
+
+  // Get clock content
+  const hour = padZeros(time.hour);
+  const minute = padZeros(time.minute);
+  const second = padZeros(time.second);
+  const date = padZeros(time.date);
+  const month = getMonthShort(time.month).toUpperCase();
+  const year = time.year;
 
   // Set clock dials degree
   document.querySelector(
@@ -57,6 +130,14 @@ const start = (time) => {
   document.querySelector(
     ".meter__dial--hour"
   ).style.transform = `rotate(${hourDialDegree}deg)`;
+
+  // Set clock content
+  document.querySelector(".meter__digital__hour").innerHTML = `${hour}`;
+  document.querySelector(".meter__digital__minute").innerHTML = `${minute}`;
+  document.querySelector(".meter__digital__second").innerHTML = `${second}`;
+  document.querySelector(
+    ".meter__digital__date"
+  ).innerHTML = `${date} ${month} ${year}`;
 };
 
 export { render, start };
--- scripts/utilities/strings.js
+++ scripts/utilities/strings.js
@@ -1,3 +1,26 @@
+const getDay = (day) => {
+  switch (day) {
+    case 0:
+      return "Sunday";
+    case 1:
+      return "Monday";
+    case 2:
+      return "Tuesday";
+    case 3:
+      return "Wednesday";
+    case 4:
+      return "Thursday";
+    case 5:
+      return "Friday";
+    case 6:
+      return "Saturday";
+  }
+};
+
+const getDayShort = (day) => {
+  return getDay(day).slice(0, 3);
+};
+
 const getDayId = (day) => {
   switch (day) {
     case 0:
@@ -15,6 +38,39 @@ const getDayId = (day) => {
     case 6:
       return "Sabtu";
   }
+};
+
+const getMonth = (month) => {
+  switch (month) {
+    case 0:
+      return "January";
+    case 1:
+      return "February";
+    case 2:
+      return "March";
+    case 3:
+      return "April";
+    case 4:
+      return "May";
+    case 5:
+      return "June";
+    case 6:
+      return "July";
+    case 7:
+      return "August";
+    case 8:
+      return "September";
+    case 9:
+      return "October";
+    case 10:
+      return "November";
+    case 11:
+      return "December";
+  }
+};
+
+const getMonthShort = (month) => {
+  return getMonth(month).slice(0, 3);
 };
 
 const getMonthId = (month) => {
@@ -50,4 +106,12 @@ const getMonthIdShort = (month) => {
   return getMonthId(month).slice(0, 3);
 };
 
-export { getDayId, getMonthId, getMonthIdShort };
+export {
+  getDay,
+  getDayShort,
+  getDayId,
+  getMonth,
+  getMonthShort,
+  getMonthId,
+  getMonthIdShort,
+};
